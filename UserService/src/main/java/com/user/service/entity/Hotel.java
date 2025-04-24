8354079ff8896672273bd0f3243c6932335ffe33
Ankit Pandit <ankitpandit1215@gmail.com>
2025-04-24 18:40:11 +0530

    Refactor services, add ServiceRegistry setup, and new Hotel entity in UserService

---
+++ UserService/src/main/java/com/user/service/entity/Hotel.java
@@ -0,0 +1,10 @@
+package com.user.service.entity;
+
+import jakarta.persistence.Column;
+
+public class Hotel {
+    private String id;
+    private String name;
+    private String location;
+    private String about;
+}
--- UserService/src/main/java/com/user/service/services/impl/UserServiceImpl.java
+++ UserService/src/main/java/com/user/service/services/impl/UserServiceImpl.java
@@ -1,12 +1,17 @@
 package com.user.service.services.impl;
 
+import com.user.service.entity.Rating;
 import com.user.service.entity.User;
 import com.user.service.exception.ResourceNotFoundException;
 import com.user.service.repository.UserRepository;
 import com.user.service.services.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
@@ -15,6 +20,11 @@ public class UserServiceImpl implements UserService {
 
     @Autowired
     private UserRepository userRepository;
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    private Logger logger = LoggerFactory.getLogger(UserServiceImpl.class);
 
 
     @Override
@@ -32,6 +42,14 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public User getUser(String userId) {
-        return userRepository.findById(userId).orElseThrow(()-> new ResourceNotFoundException("User with given id is not found on server !!" + userId));
+        //get user from database with help of user repository
+        User user = userRepository.findById(userId).orElseThrow(()-> new ResourceNotFoundException("User with given id is not found on server !!" + userId));
+
+        //fetch rating of above user from rating service
+        //http://localhost:8083/api/rating/users/5a520c2c-eb94-47f5-9fbf-42aa22ecba63
+        ArrayList<Rating> ratingOfUser = restTemplate.getForObject("http://localhost:8083/api/rating/users/" + user.getUserId(), ArrayList.class);
+        logger.info("{}",ratingOfUser);
+        user.setRatings(ratingOfUser);
+        return user;
     }
 }
