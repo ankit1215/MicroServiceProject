81654fe85b184988e5f0029ece994d066d3eaf04
Ankit Pandit <ankitpandit1215@gmail.com>
2025-04-30 15:57:03 +0530

    Circuit Breaker using Resilience4J and Rate Limiter

--- UserService/src/main/java/com/user/service/controller/UserController.java
+++ UserService/src/main/java/com/user/service/controller/UserController.java
@@ -2,6 +2,11 @@ package com.user.service.controller;
 
 import com.user.service.entity.User;
 import com.user.service.services.UserService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
+import io.github.resilience4j.retry.annotation.Retry;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +17,8 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
+
+    private static final Logger logger = LoggerFactory.getLogger(UserController.class);
 
     @Autowired
     private UserService userService;
@@ -25,10 +32,32 @@ public class UserController {
     }
 
     //get Single user id
+    int retryCount = 1;
     @GetMapping("/{userId}")
+    //@CircuitBreaker(name = "ratingHotelBreaker", fallbackMethod = "ratingHotelFallback")
+    //@Retry(name = "ratingHotelService", fallbackMethod = "ratingHotelFallback")
+    @RateLimiter(name = "userRateLimiter", fallbackMethod = "ratingHotelFallback")
     public ResponseEntity<User> getSingleUser(@PathVariable String userId){
+        logger.info("Retry Count: " + retryCount);
+        retryCount++;
         User user = userService.getUser(userId);
         return ResponseEntity.ok(user);
+    }
+
+    //create a fall back method for circuitebreaker
+
+
+    public ResponseEntity<User> ratingHotelFallback(String userId, Exception ex){
+        //logger.info("fallback is executed because service is down: ", ex.getMessage());
+        User user = User.builder()
+                .email("Dummy@gmail.com")
+                .name("Dummy Kumar")
+                .about("This user is created down becasue service is down")
+                .userId("12345")
+                .build();
+        return new ResponseEntity<>(user, HttpStatus.OK);
+
+
     }
 
     //get All Users
