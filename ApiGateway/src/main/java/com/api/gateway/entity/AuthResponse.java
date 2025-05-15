d54f22865993571d75e5cdc78b731b8fe3fb8b13
Ankit Pandit <ankitpandit1215@gmail.com>
2025-05-15 15:24:12 +0530

    Implementing Spring Security at API GATEWAY using OKTA

---
+++ ApiGateway/src/main/java/com/api/gateway/entity/AuthResponse.java
@@ -0,0 +1,21 @@
+package com.api.gateway.entity;
+
+import lombok.*;
+
+import java.util.Collection;
+
+@Data
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthResponse {
+
+    private String userId;
+    private String accessToken;
+    private String refreshToken;
+    private long expireAt;
+    private Collection<String> authorities;
+
+
+}
